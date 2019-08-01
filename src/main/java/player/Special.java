0edecbaf6e2f384f20a27dc0e8f099f3f3aef4d7
Badgy <jonaskhansen@gmail.com>
2019-08-01 23:51:24 +0200

    create obj for weapon specials

--- src/main/java/player/Special.java
+++ src/main/java/player/Special.java
@@ -1,4 +1,37 @@
 package player;
 
 public class Special {
+    private String name;
+    private double px;
+    private double py;
+    private double pw;
+    private double ph;
+
+    public Special(String name, double px, double py, double pw, double ph) {
+        this.name = name;
+        this.px = px;
+        this.py = py;
+        this.pw = pw;
+        this.ph = ph;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPx() {
+        return px;
+    }
+
+    public double getPy() {
+        return py;
+    }
+
+    public double getPw() {
+        return pw;
+    }
+
+    public double getPh() {
+        return ph;
+    }
 }
