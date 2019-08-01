f41c6701482db7a27bd5e6fdd00b5aacd554b349
Badgy <jonaskhansen@gmail.com>
2019-08-01 23:58:10 +0200

    create obj for items

--- src/main/java/player/Item.java
+++ src/main/java/player/Item.java
@@ -1,4 +1,27 @@
 package player;
 
 public class Item {
+    private int pos;
+    private String name;
+
+    public Item(int pos, String name) {
+        this.pos = pos;
+        this.name = name;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
