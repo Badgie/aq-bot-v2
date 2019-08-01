0d64a0953ca95e310b4c865c06fa83bf6da6b217
Badgy <jonaskhansen@gmail.com>
2019-08-01 23:59:27 +0200

    create obj for pets

--- src/main/java/player/Pet.java
+++ src/main/java/player/Pet.java
@@ -1,4 +1,41 @@
 package player;
 
+import common.Affinity;
+
+import java.util.List;
+
 public class Pet {
+    private int pos;
+    private String name;
+    private List<Affinity> affinities;
+
+    public Pet(int pos, String name, List<Affinity> affinities) {
+        this.pos = pos;
+        this.name = name;
+        this.affinities = affinities;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Affinity> getAffinities() {
+        return affinities;
+    }
+
+    public void setAffinities(List<Affinity> affinities) {
+        this.affinities = affinities;
+    }
 }
