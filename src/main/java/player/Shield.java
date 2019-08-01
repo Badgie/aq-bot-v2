4323bbb8ce2ce26506a13d19028f378c7956f42d
Badgy <jonaskhansen@gmail.com>
2019-08-01 23:53:45 +0200

    create obj for shields

--- src/main/java/player/Shield.java
+++ src/main/java/player/Shield.java
@@ -1,4 +1,41 @@
 package player;
 
+import common.Affinity;
+
+import java.util.List;
+
 public class Shield {
+    private int pos;
+    private String name;
+    private List<Affinity> affinities;
+
+    public Shield(int pos, String name, List<Affinity> affinities) {
+        this.pos = pos;
+        this.name = name;
+        this.affinities = affinities;
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    public void setPos(int pos) {
+        this.pos = pos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Affinity> getAffinities() {
+        return affinities;
+    }
+
+    public void setAffinities(List<Affinity> affinities) {
+        this.affinities = affinities;
+    }
 }
