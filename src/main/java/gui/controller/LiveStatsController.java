b857f7b86835be8f73adeaca1304ff5c0ac9a09b
Badgy <jonaskhansen@gmail.com>
2019-08-21 17:06:34 +0200

    setup property change listener for log, such that live stats are displayed

--- src/main/java/gui/controller/LiveStatsController.java
+++ src/main/java/gui/controller/LiveStatsController.java
@@ -1,43 +1,50 @@
 package gui.controller;
 
-import bot.Bot;
-import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
-import javafx.stage.WindowEvent;
 import log.Log;
 import util.Util;
 
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.io.IOException;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LiveStatsController implements Initializable {
+public class LiveStatsController implements Initializable, PropertyChangeListener {
 
     public Text XP_TEXT;
     public Text GOLD_TEXT;
     public Text ZTOKEN_TEXT;
     public Text DEATH_TEXT;
     public Text ENEMY_TEXT;
+    private Log LOG;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        new Thread(() -> {
-            Log log = Util.getBot().getLog();
-            XP_TEXT.setText("XP: " + log.getXp());
-            GOLD_TEXT.setText("Gold: " + log.getGold());
-            ZTOKEN_TEXT.setText("Z-Tokens: " + log.getZtoken());
-            DEATH_TEXT.setText("Deaths: " + log.getDeaths());
-            ENEMY_TEXT.setText("Enemies: " + log.getEnemies().size());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }).start();
+        LOG = Util.getBot().getLog();
+        LOG.addPropertyChangeListener(this);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        String prop = evt.getPropertyName();
+        System.out.println(prop);
+        switch (prop) {
+            case "xp":
+                XP_TEXT.setText("XP: " + LOG.getXp());
+                break;
+            case "gold":
+                GOLD_TEXT.setText("Gold: " + LOG.getGold());
+                break;
+            case "ztoken":
+                ZTOKEN_TEXT.setText("Z-Tokens: " + LOG.getZtoken());
+                break;
+            case "deaths":
+                DEATH_TEXT.setText("Deaths: " + LOG.getDeaths());
+                break;
+            case "enemies":
+                ENEMY_TEXT.setText("Enemies: " + LOG.getEnemies().size());
+                break;
+        }
     }
 }
