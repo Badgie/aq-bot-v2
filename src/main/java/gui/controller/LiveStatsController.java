7ccb4768759eecbcad2e0c57a2b8495826cf597b
Badgy <jonaskhansen@gmail.com>
2019-08-21 14:42:41 +0200

    add thread that updates stat texts

--- src/main/java/gui/controller/LiveStatsController.java
+++ src/main/java/gui/controller/LiveStatsController.java
@@ -4,7 +4,9 @@ import bot.Bot;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
 import javafx.stage.WindowEvent;
+import log.Log;
 import util.Util;
 
 import java.awt.*;
@@ -16,8 +18,26 @@ import java.util.ResourceBundle;
 
 public class LiveStatsController implements Initializable {
 
+    public Text XP_TEXT;
+    public Text GOLD_TEXT;
+    public Text ZTOKEN_TEXT;
+    public Text DEATH_TEXT;
+    public Text ENEMY_TEXT;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        new Thread(() -> {
+            Log log = Util.getBot().getLog();
+            XP_TEXT.setText("XP: " + log.getXp());
+            GOLD_TEXT.setText("Gold: " + log.getGold());
+            ZTOKEN_TEXT.setText("Z-Tokens: " + log.getZtoken());
+            DEATH_TEXT.setText("Deaths: " + log.getDeaths());
+            ENEMY_TEXT.setText("Enemies: " + log.getEnemies().size());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
