071c01d89290535c79564a86570a89b1a8f0f519
Badgy <jonaskhansen@gmail.com>
2019-08-22 12:00:28 +0200

    move armor name arrays from armor class

---
+++ src/main/java/armor/SpecialArmor.java
@@ -0,0 +1,37 @@
+package armor;
+
+/**
+ * Class containing special armor names
+ */
+class SpecialArmor {
+
+    static String[] TERROR_SET = {
+            "anxiety raiment",
+            "menace raiment",
+            "quester's anxiety raiment",
+            "guardian menace raiment",
+            "fear raiment",
+            "dread raiment",
+            "guardian dread raiment",
+            "horror raiment",
+            "despair raiment",
+            "guardian despair raiment",
+            "grim raiment",
+            "terror raiment"
+    };
+
+    static String[] NEKO = {
+            "sol neko",
+            "luna neko",
+            "advanced sol neko",
+            "cougar sol neko",
+            "lion sol neko",
+            "solid gold sol neko",
+            "advanced luna neko",
+            "leopard luna neko",
+            "tiger luna neko",
+            "solid gold luna neko",
+    };
+
+
+}
