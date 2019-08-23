349493c9982066d1c3e7367cd70fa8b029cf06d5
Badgy <jonaskhansen@gmail.com>
2019-08-23 10:54:33 +0200

    add title and cleanup code

--- src/main/java/GUIMain.java
+++ src/main/java/GUIMain.java
@@ -1,18 +1,13 @@
-import com.google.gson.JsonObject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 import util.ConfigUtil;
 import util.GUIUtil;
 import util.Util;
 
-import java.io.*;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class GUIMain extends Application {
 
@@ -37,13 +32,18 @@ public class GUIMain extends Application {
         Parent root = loader.load();
         Scene scene = new Scene(root, 400, 600);
         String os = System.getProperty("os.name");
+
         stage.setScene(scene);
+        stage.setTitle("AQ bot");
+
         GUIUtil.setIndexController(loader.getController());
         Util.setPrimaryStage(stage);
+
         ConfigUtil.firstTimeSetup();
         if (os.equals("Linux")) {
             ConfigUtil.linuxSetup();
         }
+
         stage.show();
     }
 }
